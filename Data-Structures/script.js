742239c75c2a64aec8cd177679ee71ce1bfcdb15
Ömer Faruk Gözegir <gozgirfaruk@gmail.com>
2025-02-11 17:22:21 +0300

    Lesson-Vl

--- Data-Structures/script.js
+++ Data-Structures/script.js
@@ -31,3 +31,29 @@ console.log(restaurant.location);
 
 const {fri:{open,close}}=openingHours;
 console.log(open,close)
+// SPREAD,because on RIGHT side of =
+const arr = [...restaurant.starterMenu , ...restaurant.mainMenu,'Soups'];
+console.log(arr);
+// REST, because on LEFT side of =
+const [a,b,...others] = [1,2,3,4,5];
+console.log(a,b,others);
+
+const rest1 = {
+  name:'Subway',
+  owner: 'Giovanni Rossi',
+  numGuest:0
+};
+
+const rest2={
+  name: 'KFC',
+  owner:'Anonymous'
+}
+//rest1.numGuest = rest1.numGuest ?? 20;
+rest1.numGuest||=10;
+rest2.owner = rest2.owner && 'Harland Sanders';
+
+console.log(rest1);
+console.log(rest2);
+
+for (const item of restaurant.categories.entries())
+  console.log(item);
